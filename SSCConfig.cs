bc97b01154abf6020ce165fe17748c39845728a3
zzp198 <zzp198@163.com>
2022-07-07 22:09:08 +0800

    v0.1

--- SSCConfig.cs
+++ SSCConfig.cs
@@ -1,10 +1,37 @@
+using System.Collections.Generic;
 using System.ComponentModel;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
 using Terraria.ModLoader.Config;
 
-namespace SSC
+namespace ServerMod
 {
     public class SSCConfig : ModConfig
     {
+        [Label("StartingInventory")] public List<ItemDefinition> StartingInventory = new()
+        {
+            new ItemDefinition(3507),
+            new ItemDefinition(3509),
+            new ItemDefinition(3506),
+        };
+
         public override ConfigScope Mode => ConfigScope.ServerSide;
+
+        [Label("StartingLife")]
+        [Tooltip("StartingLife")]
+        [Range(20, 400)]
+        [Increment(20)]
+        [Slider]
+        [DefaultValue(100)]
+        public int StartingLife { get; set; }
+
+        [Label("StartingMana")]
+        [Tooltip("StartingMana")]
+        [Range(0, 200)]
+        [Increment(20)]
+        [Slider]
+        [DefaultValue(20)]
+        public int StartingMana { get; set; }
     }
 }
