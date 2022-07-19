b28cbb1720804798b5707e0ef4c8b0fe4e64de44
zzp198 <zzp198@163.com>
2022-07-20 05:03:43 +0800

    Reset LP Code.

--- Common/Player/SSCPlayer.cs
+++ Common/Player/SSCPlayer.cs
@@ -1,7 +1,6 @@
 using System.IO;
 using Terraria;
-using Terraria.DataStructures;
-using Terraria.ID;
+using Terraria.IO;
 using Terraria.ModLoader;
 
 namespace SSC.Common.Player;
@@ -10,39 +9,17 @@ public class SSCPlayer : ModPlayer
 {
     public override void OnEnterWorld(Terraria.Player player)
     {
-        if (Main.netMode == NetmodeID.MultiplayerClient)
+        Directory.Delete(SSC.CachePath, true);
+        Directory.CreateDirectory(SSC.CachePath);
+        Terraria.Player.SavePlayer(new PlayerFileData(Path.Combine(SSC.CachePath, "Anonymous.plr"), false)
         {
-            var dir = Path.Combine(SSC.SavePath, "Cache");
-            Directory.CreateDirectory(dir);
-            File.WriteAllBytes(Path.Combine(dir, "Guest.plr"), ModContent.GetFileBytes("SSC/Guest.plr"));
-            File.WriteAllBytes(Path.Combine(dir, "Guest.tplr"), ModContent.GetFileBytes("SSC/Guest.tplr"));
-
-            Terraria.Player.LoadPlayer(Path.Combine(dir, "Guest.plr"), false).SetAsActive();
-            Main.LocalPlayer.Spawn(PlayerSpawnContext.SpawningIntoWorld);
-            Main.LocalPlayer.KillMe(PlayerDeathReason.LegacyEmpty(), 0, 0);
-
-            var packet = Mod.GetPacket();
-            packet.Write((int)PacketID.UUID);
-            packet.Write(Main.clientUUID);
-            packet.Send();
-        }
-
-
-        // if (this.whoAmI != Main.myPlayer)
-        // {
-        //     int num1 = (int) ((double) this.position.X + (double) (this.width / 2)) / 16;
-        //     int num2 = (int) ((double) this.position.Y + (double) (this.height / 2)) / 16;
-        //     if (Main.netMode == 1 && !Main.sectionManager.TilesLoaded(num1 - 3, num2 - 3, num1 + 3, num2 + 3))
-        //         flag1 = true;
-        //     if (flag1)
-        //     {
-        //         this.outOfRange = true;
-        //         this.numMinions = 0;
-        //         this.slotsMinions = 0.0f;
-        //         this.itemAnimation = 0;
-        //         this.UpdateBuffs(i);
-        //         this.PlayerFrame();
-        //     }
-        // }
+            // this.Name = this.Player.name
+            Metadata = FileMetadata.FromCurrentSettings(FileType.Player),
+            Player = new Terraria.Player
+            {
+                name = "Anonymous",
+                difficulty = (byte)Main.GameMode // Correspond to each other and can be used directly.
+            }
+        }, true);
     }
 }
--- SSC.cs
+++ SSC.cs
@@ -1,87 +1,26 @@
 using System.IO;
-using SSC.Common.System;
 using Terraria;
-using Terraria.ID;
 using Terraria.ModLoader;
-using Terraria.ModLoader.IO;
 
 namespace SSC
 {
     public class SSC : Mod
     {
+        // this._player.name = "";
+        // Main.clrInput();
+        // UIVirtualKeyboard state = new UIVirtualKeyboard(Lang.menu[45].Value, "", new UIVirtualKeyboard.KeyboardSubmitEvent(this.OnFinishedNamingAndCreating), new Action(this.OnCancledNaming));
+        // state.SetMaxInputLength(20);
+        // Main.MenuUI.SetState((UIState) state);
+        
         public static string SavePath = Path.Combine(Main.SavePath, "SSC");
         public static string CachePath = Path.Combine(SavePath, "Cache");
 
         public override void HandlePacket(BinaryReader br, int _)
         {
-            switch (Main.netMode)
-            {
-                case NetmodeID.MultiplayerClient:
-                    ClientPacket(br, _);
-                    break;
-                case NetmodeID.Server:
-                    ServerPacket(br, _);
-                    break;
-            }
-        }
-
-        void ClientPacket(BinaryReader br, int _)
-        {
-            var type = (PacketID)br.Read();
-            if (type == PacketID.SSCList)
-            {
-                var cacheDir = Path.Combine(SavePath, "Cache");
-                var count = br.Read();
-                Directory.Delete(cacheDir, true);
-                Directory.CreateDirectory(cacheDir);
-                for (var i = 0; i < count; i++)
-                {
-                    var compound = (TagCompound)TagIO.ReadTag(br, out var name);
-                    File.WriteAllBytes(Path.Combine(cacheDir, name + ".plr"), compound.GetByteArray("plr"));
-                    if (compound.ContainsKey("tplr"))
-                    {
-                        File.WriteAllBytes(Path.Combine(cacheDir, name + ".tplr"), compound.GetByteArray("tplr"));
-                    }
-                }
-
-                var UISystem = ModContent.GetInstance<UISystem>();
-                UISystem.UI.SetState(UISystem.UIState);
-                return;
-            }
-        }
-
-        void ServerPacket(BinaryReader br, int _)
-        {
-            var type = (PacketID)br.Read();
-            if (type == PacketID.UUID)
-            {
-                var players = Directory.GetFiles(Path.Combine(SavePath, br.ReadString()), "*.plr");
-
-                var packet = GetPacket();
-                packet.Write((int)PacketID.SSCList);
-                packet.Write(players.Length);
-                foreach (var player in players)
-                {
-                    var compound = new TagCompound
-                    {
-                        { "plr", File.ReadAllBytes(player) }
-                    };
-                    var tplr = Path.ChangeExtension(player, ".tplr");
-                    if (File.Exists(tplr))
-                    {
-                        compound.Add("tplr", File.ReadAllBytes(tplr));
-                    }
-
-                    TagIO.WriteTag(Path.GetFileNameWithoutExtension(player), compound, packet);
-                }
-
-                packet.Send(_);
-                return;
-            }
         }
     }
 
-    public enum PacketID
+    public enum PKG_ID
     {
         UUID, // C-S, UUID
         SSCList, // S-C, Count,[Size, TagCompoent]
